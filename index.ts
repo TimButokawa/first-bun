e8235e3149447701f32bca912fff37aa2f7b3fd0
Tim Butorac <timbutorac@gmail.com>
2023-10-14 08:53:02 -0700

    routes n err
    script to watch index

--- index.ts
+++ index.ts
@@ -1,5 +1,31 @@
-const hello = (name: 'sally' | 'bill') => {
-  return `Hello ${name}!`;
-}
+const port = 3000;
 
-console.log(hello('sally'));
+const server = Bun.serve({
+  port,
+  fetch(req) {
+    const url = new URL(req.url)
+
+    if (url.pathname === '/') {
+      return new Response('Bun server home!');
+    }
+
+    // lil err
+    if (url.pathname === '/about') {
+      throw new Error('There was an error!');
+    }
+
+    return new Response('Not Found!');
+  },
+  // handle lil err
+  error(err) {
+    return new Response(
+      `<pre>${err.message}</pre><br/><pre>${err.stack}</pre>`, {
+        headers: {
+          'Content-Type': 'text/html',
+        },
+      },
+    );
+  }
+});
+
+console.log(`listening on localhost:${server.port}`);
